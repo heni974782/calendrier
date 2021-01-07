c18b81b3571dddb10960e0939876d28e0f760105
Heni Rekik <heni.rekik8@gmail.com>
2021-01-07 19:04:08 +0100

    screen shot

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+
+import { NgCalendarModule  } from 'ionic2-calendar'; // module de calendrier de ionic/angular
+
+import { HomePageRoutingModule } from './home-routing.module';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule,
+    NgCalendarModule,
+  ],
+ 
+  declarations: [HomePage]
+})
+export class HomePageModule {}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {  
+  production: true,
+
+  firebase : {  // relier notre calendrier avec notre backend choisie
+    apiKey: "AIzaSyDdohrlTYhGKSqsideMImMDUbS8cZj3NZk",
+  authDomain: "ionic-4-custom-calendar-d260a.firebaseapp.com",
+  projectId: "ionic-4-custom-calendar-d260a",
+  storageBucket: "ionic-4-custom-calendar-d260a.appspot.com",
+  messagingSenderId: "778128091284",
+  appId: "1:778128091284:web:341d33e1cc9b8c545aec81",
+  }
+};
